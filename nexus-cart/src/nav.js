521773215d1e02321bc75ff5fea773863d918a73
Aman-Goswami05 <amangoswami5570@gmail.com>
2024-01-15 16:55:03 +0530

    Product cart page added!

--- nexus-cart/src/nav.js
+++ nexus-cart/src/nav.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaSearch,FaUserCircle,FaCartArrowDown } from "react-icons/fa";
+import { ImBin2 } from "react-icons/im";
+import { MdShoppingCartCheckout } from "react-icons/md";
+
 import { Link } from "react-router-dom";
 import './nav.css';
+
+const ShowCart = () => {
+    let cart = document.querySelector('.shopping-cart');
+    cart.classList.toggle('active');
+}
+
 const Nav = () => {
     const dataToSend = "Hii from Aman";
     return (
@@ -41,13 +50,42 @@ const Nav = () => {
                             <Link to="/login"><FaUserCircle size={20} /></Link>
                         </li>
                         <li>
-                            <a href=""><FaCartArrowDown size={20} /></a>
+                            <button onClick={ShowCart}><FaCartArrowDown size={20} /></button>
                         </li>
                     </ul>
                 </nav>
                 <label for="nav_check" className="hamburger">
                     <GiHamburgerMenu size={20} />
                 </label>
+
+                <div className="shopping-cart">
+                    <div className="cart-box">
+                        <ImBin2 size={20} className="bin-btn"/>
+                        <img src="/images/logo.png" alt=""></img>
+                        <div className="cart-content">
+                            <h3 className="cart-product-title">Woodland Shoe</h3>
+                            <span className="cart-product-price">$120.00/-</span>
+                            <span className="cart-product-qty">Qty : 1</span>
+                        </div>
+                    </div>
+                    <div className="cart-box">
+                        <ImBin2 size={20} className="bin-btn"/>
+                        <img src="/images/slippers/slippers3.png" alt=""></img>
+                        <div className="cart-content">
+                            <h3 className="cart-product-title">Woodland Shoe</h3>
+                            <span className="cart-product-price">$120.00/-</span>
+                            <span className="cart-product-qty">Qty : 2</span>
+                        </div>
+                    </div>
+                    <div className="cart-total">
+                        Total Amount : $360.00/- 
+                    </div>
+                    <Link className="cart-checkout-btn"to={`/Checkout`}>
+                        Checkout
+                        <MdShoppingCartCheckout size={20}/>
+                    </Link>
+                </div>
+
             </header>
         </>
     )
